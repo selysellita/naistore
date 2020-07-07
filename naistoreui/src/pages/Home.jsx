75d6e5ae1d23021127e05117c2c71806be6809e8
selysellita <60500591+selysellita@users.noreply.github.com>
2020-07-07 18:51:04 +0700

    asal

---
+++ naistoreui/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                Ini Home
+                Home adalah home. tempat istirahat
+            </div>
+        );
+    }
+}
+ 
+export default Home;
